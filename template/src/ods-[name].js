d025b5322d898cabcff74f9c5d3751a46530f8b0
Dale Sande <dale.sande@alaskaair.com>
2019-07-17 17:11:28 -0700

    fix: update ods wc template for shape properties css inclusion

--- template/src/ods-[name].js
+++ template/src/ods-[name].js
@@ -6,41 +6,40 @@
 import { LitElement, html } from "lit-element";
 
 // Import touch detection lib
-// import 'focus-visible/dist/focus-visible.min.js';
+import 'focus-visible/dist/focus-visible.min.js';
 // impot the processed CSS file into the scope of the component
+import componentProperties from './tokens/componentShapeProperties-css.js';
 import styleCss from "./style-css.js";
 
 // build the component class
 class Ods[Name] extends LitElement {
-  constructor() {
-    super();
-    this.cssClass = "";
+  // constructor() {
+  //   super();
 
-    /*
-      If the component requires a touch detection,
-      please use this function to determine if a user is
-      activelly touching a device, versus detecting if
-      the device is touych enables or a handheld device.
+  //   /*
+  //     If the component requires a touch detection,
+  //     please use this function to determine if a user is
+  //     activelly touching a device, versus detecting if
+  //     the device is touych enables or a handheld device.
 
-      Also uncomment the touch detection lib above
-    */
-    // this.addEventListener('touchstart', function() {
-    //   this.classList.add('is-touching');
-    // });
-  }
+  //     Also uncomment the touch detection lib above
+  //   */
+  //   this.addEventListener('touchstart', function() {
+  //     this.classList.add('is-touching');
+  //   });
+  // }
 
   // function to define props used within the scope of thie component
   static get properties() {
     return {
-      cssClass: {
-        type: String
-      }
+      cssClass:   { type: String }
     };
   }
 
   // function that renders the HTML and CSS into  the scope of the component
   render() {
     return html`
+      ${componentProperties}
       ${styleCss}
 
       <div class=${this.cssClass}>
