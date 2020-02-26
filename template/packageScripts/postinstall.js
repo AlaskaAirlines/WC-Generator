2218bf9bcbd87183fd12a987568f7e82809c0971
Dale Sande <dale.sande@alaskaair.com>
2020-02-25 20:47:39 -0800

    fix: add postinstall script

---
+++ template/packageScripts/postinstall.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const chalk = require('chalk');
+const pjson = require('../package.json');
+
+console.log(chalk.hex('#f26135')(`
+╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮
+
+        Thanks for installing the latest version
+                of `) + chalk.hex('#ffd200').bold(`auro-[name] v${pjson.version}.`) + chalk.hex('#f26135')(`
+
+╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
+`)
+);
