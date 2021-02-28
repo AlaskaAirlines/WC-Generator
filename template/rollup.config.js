1af84733d671c419cca7f79da9c86c7540ebb2cf
Dale Sande <dale.sande@gmail.com>
2021-02-27 21:58:07 -0800

    fix: update rollup to have default multi-element pattern

--- template/rollup.config.js
+++ template/rollup.config.js
@@ -26,10 +26,12 @@ const getSharedPlugins = (isLegacy) => [
 ];
 
 const modernConfig = {
-  input: 'src/[namespace]-[name].js',
+  input: {
+    ['[namespace]-[name]__bundled']: './src/[namespace]-[name].js',
+  },
   output: {
     format: 'esm',
-    file: 'dist/[namespace]-[name]__bundled.js'
+    dir: 'dist/'
   },
   plugins: [
     // remove shady DOM polyfill for modern browsers
@@ -51,7 +53,7 @@ const modernConfig = {
   ]
 };
 
-const legacyConfig = {
+const [namespace][Name]Config = {
   input: 'src/es5.js',
   output: {
     format: 'iife',
@@ -60,4 +62,4 @@ const legacyConfig = {
   plugins: getSharedPlugins(true)
 };
 
-export default [modernConfig, legacyConfig];
+export default [modernConfig, [namespace][Name]];
