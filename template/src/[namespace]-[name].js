d3145f7281588699b2a8e33d46e38841ea44e453
Dale Sande <dale.sande@gmail.com>
2021-05-10 14:42:45 -0700

    refactor(focus-visible support): update based on feedback

--- template/src/[namespace]-[name].js
+++ template/src/[namespace]-[name].js
@@ -15,7 +15,6 @@ import { LitElement, html } from "lit-element";
 import "focus-visible/dist/focus-visible.min.js";
 import styleCss from "./style-css.js";
 import styleCssFixed from './style-fixed-css.js';
-import { isFocusVisibleSupported, isFocusVisiblePolyfillAvailable } from './util';
 
 // See https://git.io/JJ6SJ for "How to document your components using JSDoc"
 /**
@@ -27,12 +26,9 @@ import { isFocusVisibleSupported, isFocusVisiblePolyfillAvailable } from './util
 
 // build the component class
 class [Namespace][Name] extends LitElement {
-  constructor() {
-    super();
-    if (!isFocusVisibleSupported() && isFocusVisiblePolyfillAvailable()) {
-      window.applyFocusVisiblePolyfill(this.shadowRoot);
-    }
-  }
+  // constructor() {
+  //   super();
+  // }
 
   // function to define props used within the scope of this component
   static get properties() {
@@ -55,6 +51,8 @@ class [Namespace][Name] extends LitElement {
   // function that renders the HTML and CSS into  the scope of the component
   render() {
     return html`
+
+      <!-- this is demo code, DO NOT USE IN YOUR ELEMENT -->
       <div class=${this.cssClass} tabindex="0">
         <slot></slot>
       </div>
