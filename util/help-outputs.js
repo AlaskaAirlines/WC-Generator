e8722ae2fffce4ab141874b83b71c1632aa5739c
Dale Sande <dale.sande@gmail.com>
2021-05-17 22:50:49 -0700

    feat(version help): add support for showing installed version with --help

--- util/help-outputs.js
+++ util/help-outputs.js
@@ -1,11 +1,12 @@
 const chalk = require('chalk');
+const pjson = require('../package.json');
 
 const generate = `
     ${chalk.gray('Usage:')}
     ${chalk.bold('wc-generator [options]')}
 
     ${chalk.gray('Example:')}
-    ${chalk.bold('wc-generator --name dialog')}
+    ${chalk.bold('wc-generator --name auro-dialog')}
 
     ${chalk.gray('Description:')}
     Generate a new Auro Web Component from template.
@@ -14,14 +15,20 @@ const generate = `
     For more help with building an Auro Web Component,
     be sure to see ${chalk.blue('https://bit.ly/3cqTsp5')}
 
+    ${chalk.gray('Installed version:')}
+    ${pjson.version}
+
+    ${chalk.gray('Upgrade to latest version:')}
+    $ npm i @alaskaairux/wc-generator@latest -g
+
     ${chalk.gray('Options:')}
 
-    -h, --help                      Get help info about WC generator
-    -t, --test                      Test repo generation without installing dependencies
-    -n, --name [name]               Name of the web component you wish to build, must follow format [namespace]-[name]
-    -P, --npm [npm]                 Choose npm namespace
-    -d, --dir [directory]           Directory where the new custom element files will be created
-    --verbose                       Verbose command line feedback
+    -h, --help                Get help info about WC generator
+    -t, --test                Test repo generation without installing dependencies
+    -n, --name [name]         Name of the web component to build, must follow format [namespace]-[name]
+    -P, --npm [npm]           Choose npm namespace
+    -d, --dir [directory]     Directory where the new custom element will be created
+    --verbose                 Verbose command line feedback
 `;
 
 module.exports = { generate };
