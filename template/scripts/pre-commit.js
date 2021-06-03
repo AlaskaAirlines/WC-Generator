393d8c266e8a90e627fff88ce22f7b37893eb50a
Dale Sande <dale.sande@gmail.com>
2021-06-03 10:13:08 -0700

    fix(pre-commit): replace question with statement

---
+++ template/scripts/pre-commit.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const chalk = require('chalk');
+console.log(chalk.hex('#ffd200')(`
+
+╭ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ──────────────────────────────╮`) + chalk.hex('#f26135')(`
+
+    Are you familiar with Auro's Definition of Done?
+
+                Please be sure to review`) + chalk.hex('#ffd200')(`
+      https://auro.alaskaair.com/definition-of-done`) + chalk.hex('#f26135')(`
+            before submitting your pull request
+             to ensure that you are compliant.`) + chalk.hex('#ffd200')(`
+
+╰─────────────────────────────── ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─╯
+`)
+);
