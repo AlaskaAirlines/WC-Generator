044ec4d89b5c0b39bc286783f4d2019a20e16c36
Dale Sande <dale.sande@gmail.com>
2021-06-30 10:08:13 -0700

    refactor(migration): update migration script and README

--- util/auroMigrate.sh
+++ util/auroMigrate.sh
@@ -2,7 +2,7 @@
 ## Generate a new baseline install of an auro component
 # Argument expected: name of new repo
 function generateRepo {
-  command wc-generate --test --name "$1" --npm "$2"
+  command wc-generate --test --name "$1" --npm "${2//\/}"
 }
 
 ## Migrade files from legacy repo to new build
@@ -14,54 +14,57 @@ function auroMigrate {
   NC='\033[0m' # No Color
 
   command clear
-  echo -e "The following steps have been addressed to migrade code from ${YELLOW}'$1'${NC} to ${YELLOW}'$2'${NC}.\n"
-  echo -e "Please be sure to review the commit to the ${GREEN}repoUpgrde${NC} branch,\nthis migration ${YELLOW}does not${NC} account for legacy repositoy customizations\nsuch as additional dependencies or multiple CDN packaged bundles."
+  echo -e "\nThe following steps have been addressed to migrade code from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}'${NC}.\n"
+  echo -e "Please be sure to review the full commit to the ${GREEN}repoUpgrde${NC} branch,\nthis migration ${YELLOW}does not${NC} account for legacy repositoy customizations\nsuch as additional dependencies or multiple CDN packaged bundles."
 
   # migrate git dir
-  command cp -r "$1"/.git/ "$2"/.git
-  echo -e "\n${GREEN}1.${NC} ./.git directory copied from ${YELLOW}'$1'${NC} to ${YELLOW}'$2'${NC}"
+  command cp -r "${1//\/}"/.git/ "${2//\/}"/.git
+  echo -e "\n${GREEN}1.${NC} ${YELLOW}./.git${NC} directory copied from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}'${NC}"
   sleep 2
 
   # migrate src dir
-  command cp -r "$1"/src/ "$2"/src
-  echo -e "${GREEN}2.${NC} ./src directory copied from ${YELLOW}'$1'${NC} to ${YELLOW}'$2'${NC}"
+  command cp -r "${1//\/}"/src/ "${2//\/}"/src
+  echo -e "${GREEN}2.${NC} ${YELLOW}./src${NC} directory copied from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}'${NC}"
   sleep 1
 
   # migrate tests
-  command cp -r "$1"/test/ "$2"/test
-  echo -e "${GREEN}3.${NC} ./test directory copied from ${YELLOW}'$1'${NC} to ${YELLOW}'$2'${NC}"
+  command cp -r "${1//\/}"/test/ "${2//\/}"/test
+  echo -e "${GREEN}3.${NC} ${YELLOW}./test${NC} directory copied from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}'${NC}"
   sleep 1
 
   # migrate changelog file
-  command cp  "$1"/CHANGELOG.md "$2"/CHANGELOG.md
-  echo -e "${GREEN}4.${NC} ./CHANGELOG.md copied from ${YELLOW}'$1'${NC} to ${YELLOW}'$2'${NC}"
+  command cp  "${1//\/}"/CHANGELOG.md "${2//\/}"/CHANGELOG.md
+  echo -e "${GREEN}4.${NC} ${YELLOW}./CHANGELOG.md${NC} copied from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}'${NC}"
   sleep 1
 
   # move README
-  command cp "$1"/README.md "$2"/README.legacy
-  echo -e "${GREEN}5.${NC} ./README copied from ${YELLOW}'$1'${NC} to ${YELLOW}'$2'${NC}, renamed to ${YELLOW}'README.legacy'${NC}."
+  command mkdir "${2//\/}"/_legacy
+  command cp "${1//\/}"/README.md "${2//\/}"/_legacy/README.md
+  echo -e "${GREEN}5.${NC} ${YELLOW}./README${NC} copied from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}./_legacy/README.md'${NC}."
   sleep 1
 
   # move package.json
-  command cp "$1"/package.json "$2"/package.legacy
-  echo -e "${GREEN}6.${NC} ./package.json copied from ${YELLOW}'$1'${NC} to ${YELLOW}'$2'${NC}, renamed to ${YELLOW}'package.legacy'${NC}."
+  command cp "${1//\/}"/package.json "${2//\/}"/_legacy/package.json
+  echo -e "${GREEN}6.${NC} ${YELLOW}./package.json${NC} copied from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}./_legacy/package.json${NC}'."
   sleep 1
 
   # check for --no-demo
   if [[ $3 = "--no-demo" ]]; then
-    command cp "$1"/demo/index.html "$2"/demo/index.legacy
-    echo -e "${RED}7.${NC} the file ${RED}`$1/demo/index.html`${NC} was moved to ${RED}`$2/demo/index.legacy${NC}`\n"
+    command mkdir "${2//\/}"/demo/_legacy
+    # command pwd
+    command cp "${1//\/}"/demo/index.html "${2//\/}"/demo/_legacy/index.html
+    echo -e "${GREEN}7.${NC} ${YELLOW}./demo/index.html${NC} copied from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}/demo/_legacy/index.html${NC}'.\n"
     sleep 3
   else
-    command cp "$1"/demo/demo.md "$2"/demo/demo.md
-    command cp "$1"/demo/index.html "$2"/demo/index.html
-    echo -e "${GREEN}7.${NC} ./demo directory copied from ${YELLOW}'$1'${NC} to ${YELLOW}'$2'${NC}\n"
+    command cp "${1//\/}"/demo/demo.md "${2//\/}"/demo/demo.md
+    command cp "${1//\/}"/demo/index.html "${2//\/}"/demo/index.html
+    echo -e "${GREEN}7.${NC} ${YELLOW}./demo${NC} directory copied from ${YELLOW}'${1//\/}'${NC} to ${YELLOW}'${2//\/}'${NC}\n"
     sleep 3
   fi
 
   # change directory into new repo
-  command cd "$2"
-  echo -e "Switching into ${GREEN}'$2'${NC} directory\n"
+  command cd "${2//\/}"
+  echo -e "Switching into ${GREEN}'${2//\/}'${NC} directory\n"
   sleep 2
 
   # check for legacy master branch
@@ -100,7 +103,7 @@ function auroMigrate {
 
   # close out message
   echo -e "\n\nNote: The legacy ${YELLOW}README${NC} and ${YELLOW}package.json${NC} files were maintained.\nBe sure to review these files, manually update with information\nthat needs to be retained and then remove them from the project.\n"
-  echo -e "To see diffs, consider using ${YELLOW}$ vimdiff package.legacy package.json${NC}"
+  echo -e "To see diffs, consider using ${YELLOW}$ vimdiff _legacy/package.json package.json${NC}"
   echo -e "\n\nCommitted all changes to ${YELLOW}repoUpgrde${NC} Git branch\n"
   echo -e "For any subsequent changes to the ${YELLOW}repoUpgrde${NC}, please\nbe sure to amend to the previous commit.\n"
   echo -e "Use ${YELLOW}$ git commit --amend --no-edit${NC} after staging the updates.\n\n"
