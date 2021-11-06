54a4e97cddefb226b5eddd796cf5ff2d496c2a1f
Dale Sande <dale.sande@gmail.com>
2021-11-05 18:12:06 -0700

    refactor(postcss): add automation for post processing CSS files #253

--- template/scripts/postCss.js
+++ template/scripts/postCss.js
@@ -5,34 +5,73 @@ const removeNonRem = require('./removeNonRemPlugin.js');
 const postcssCustomProperties = require('postcss-custom-properties');
 const removeRules = require('postcss-remove-rules');
 const comments = require('postcss-discard-comments');
+const path = require('path');
 const fs = require('fs');
+const directoryPath = path.join(__dirname, '../src');
 
-const cssFiles = ['style']
-
-cssFiles.map(name => fs.readFile(`src/${name}.css`, (err, css) => {
-  postcss([autoprefixer, postcssCustomProperties, comments])
-    .use(comments({
-      remove: function(comment) { return comment[0] == "@"; }
-    }))
-    .use(removeRules({
-      rulesToRemove: {
-        ':root': '*'
+/**
+ * Default postCSS run
+ */
+fs.readdir(directoryPath, function (err, files) {
+  //handling error
+  if (err) {
+      return console.log('Unable to scan directory: ' + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+      if (file.includes(".css") && !file.includes("-fixed.css")) {
+        standardProcessor(file);
       }
-    }))
-    .process(css, { from: `src/${name}.css`, to: `src/${name}.css` })
-    .then(result => {
-      fs.writeFile(`src/${name}.css`, result.css, () => true)
+  });
+});
+
+/**
+ * The standardProcessor function applies tokens for fallback selectors
+ * and completes a post cleanup.
+ * @param {string} file
+ */
+function standardProcessor(file) {
+  fs.readFile(`src/${file}`, (err, css) => {
+    postcss([autoprefixer, postcssCustomProperties, comments])
+      .use(comments({
+        remove: function(comment) { return comment[0] == "@"; }
+      }))
+      .use(removeRules({
+        rulesToRemove: {
+          ':root': '*'
+        }
+      }))
+      .process(css, { from: `src/${file}`, to: `src/${file}` })
+      .then(result => {
+        fs.writeFile(`src/${file}`, result.css, () => true)
+      })
     })
-  }));
+}
 
-/*
-  Output a "fixed" stylesheet that only contains declarations with rem units
-  converted to their px equivalent.
-*/
-const cssFixedFiles = ['style-fixed']
+/**
+ * Addresses postCSS run of style-fixed files.
+ */
+fs.readdir(directoryPath, function (err, files) {
+  //handling error
+  if (err) {
+      return console.log('Unable to scan directory: ' + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+      if (file.includes("-fixed.css")) {
+        fixedProcessor(file);
+      }
+  });
+});
 
-cssFixedFiles.map(name => fs.readFile(`src/${name}.css`, (err, css) => {
-  fs.readFile(`src/${name}.css`, (err, css) => {
+
+/**
+ * Output a "fixed" stylesheet that only contains
+ * declarations with rem units converted to their px equivalent.
+ * @param {string} file
+ */
+ function fixedProcessor(file) {
+  fs.readFile(`src/${file}`, (err, css) => {
     postcss([
       autoprefixer,
       postcssCustomProperties({preserve: false}),
@@ -43,9 +82,9 @@ cssFixedFiles.map(name => fs.readFile(`src/${name}.css`, (err, css) => {
     .use(comments({
       remove: function(comment) { return comment[0] == "@"; }
     }))
-    .process(css, { from: `src/${name}.css`, to: `src/${name}.css` })
+    .process(css, { from: `src/${file}`, to: `src/${file}` })
     .then(result => {
-      fs.writeFile(`src/${name}.css`, result.css, () => true)
+      fs.writeFile(`src/${file}`, result.css, () => true)
     })
   });
-}));
+}
